0e00678f1096a20ee8140f4e3fc32d3340641f7d
Zenon2000 <puylongarjie@gmail.com>
2024-12-06 05:45:44 +0800

    resotered built version

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'arjieportfolio',
-  location: 'us-central1'
-};
-
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AboutMe from "../AboutMe";
 import Contact from "../Contact";
 import Footer from "../Footer";
